c5a7a6e5dc7db384ff05bd8429fed4ce18623d97
Pasci <hausberger.pascal@gmail.com>
2019-03-04 22:22:32 +0100

    feat: comments looking a bit better

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -16,22 +16,52 @@ class Comment extends Component {
   render(){
     const { comment, onClickingEdit } = this.props
     return (
-      <div className='comment w-100 my-3'>
-        <div className=''>
-          <div className=' bg-success'>
+      // <div className='comment w-100 my-3'>
+      //   <div className=''>
+      //     <div className=' bg-success'>
+      //       {comment.author} commented on {moment(comment.timestamp).format('MMM Do YYYY')}
+      //     </div>
+      //     <div className='row voting py-3'>
+      //       <div className='col d-flex justify-content-end align-items-center'>
+      //         <div>{comment.voteScore}</div>
+      //       </div>
+      //       <div className='col'>
+      //         <div className='row flex-column align-items-start'>
+      //           <button onClick={(e) => this.handleCommentVote(e, comment.id, 'upVote')} className='btn bg-warning'><FiChevronUp /></button>
+      //           <button onClick={(e) => this.handleCommentVote(e, comment.id, 'downVote')} className='btn bg-warning'><FiChevronDown /></button>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      //   <div className='flex-column'>
+      //     <div className='py-3'>{comment.body}</div>
+      //     <button onClick={onClickingEdit} type='button' className='btn bg-link'>Edit Comment</button>
+      //   </div>
+      // </div>
+      <div className='comment row py-3'>
+        <div className='col-11 bg-primary'>
+          <div className='bg-secondary'>
             {comment.author} commented on {moment(comment.timestamp).format('MMM Do YYYY')}
           </div>
-          <div className='voting'>
-            <div className='row flex-column'>
-              <button onClick={(e) => this.handleCommentVote(e, comment.id, 'upVote')} className='btn'><FiChevronUp /></button>
-              <button onClick={(e) => this.handleCommentVote(e, comment.id, 'downVote')} className='btn'><FiChevronDown /></button>
-              <div>{comment.voteScore}</div>
-            </div>
+          <div className='bg-danger py-4'>
+            {comment.body}
+          </div>
+          <div>
+            <button onClick={onClickingEdit} type='button' className='btn bg-link'>Edit Comment</button>
           </div>
         </div>
-        <div className='flex-column'>
-          <div className='py-3'>{comment.body}</div>
-          <button onClick={onClickingEdit} type='button' className='btn bg-link'>Edit Comment</button>
+        <div className='col-1 bg-success'>
+          <div className='row voting py-3'>
+            <div className='col'>
+              <div className='row flex-column justify-content-center align-items-center'>
+                <button onClick={(e) => this.handleCommentVote(e, comment.id, 'upVote')} className='btn bg-warning'><FiChevronUp /></button>
+                <button onClick={(e) => this.handleCommentVote(e, comment.id, 'downVote')} className='btn bg-warning'><FiChevronDown /></button>
+              </div>
+              <div className='col d-flex justify-content-center align-items-center'>
+                <div>{comment.voteScore}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
