940a80c80fbc81c6e2efa3f0d20e4b71309caac5
Pasci <hausberger.pascal@gmail.com>
2019-03-05 16:57:58 +0100

    style: cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -30,73 +30,6 @@ class App extends Component {
     this.props.getInitialData()
   }
 
-  testAPI = () => {
-    const testID = "8xf0y6ziyjabvozdd253nd"
-    const voteOption = "upVote"
-    const commentID = "894tuq4ut84ut8v4t8wun89g"
-
-    CategoriesAPI()
-      .then(response => console.log('Response Categories: ', response))
-
-    PostsAPI.getAllPosts()
-      .then(response => console.log('Response Posts: ', response))
-
-    PostsAPI.getPostsForCategory('redux')
-      .then(response => console.log('Response Posts for Category: ', response))
-
-    PostsAPI.getPost(testID)
-      .then(response => console.log('Response Single Post: ', response))
-
-    let newPost = {
-      id: '007',
-      timestamp: Date.now(),
-      title: 'New Post Title',
-      body: 'New Post Body dude',
-      author: 'Michael Scarn',
-      category: 'redux'
-    }
-
-    PostsAPI.addPost(newPost)
-      .then(response => console.log('Response Add Post: ', response))
-
-    PostsAPI.votePost(testID, voteOption)
-      .then(response => console.log('Response Vote Post: ', response))
-
-    PostsAPI.editPost(testID, 'BETTER TITLE', 'BETTER BODY for this post, mofo!')
-      .then(response => console.log('Response Edit Post: ', response))
-
-    // PostsAPI.deletePost('007')
-    //   .then(response => console.log('Response Delete Post: ', response))
-
-
-    //Comments
-    CommentsAPI.getCommentsForPost(testID)
-      .then(response => console.log('Response Get Comments: ', response))
-
-    const newComment = {
-      id: '008',
-      timestamp: Date.now(),
-      body: "Yo, I'm a comment, dude!",
-      author: 'Michael Scarn',
-      parentId: testID,
-    }
-
-    // CommentsAPI.addComment(newComment)
-    //   .then(response => console.log('Response Add Comments: ', response))
-
-    CommentsAPI.getComment(commentID)
-      .then(response => console.log('Response Get Single Comment: ', response))
-
-    CommentsAPI.voteComment(commentID, voteOption)
-      .then(response => console.log('Response Vote Comment: ', response))
-
-    CommentsAPI.editComment(commentID, Date.now(), 'YOOO NEW COMMENT, DUDE')
-      .then(response => console.log('Response Edit Comment: ', response))
-
-    CommentsAPI.deleteComment('008')
-      .then(response => console.log('Response Delete Comment: ', response))
-  }
-
   render() {
     let { isLoading, isValidCategory, isValidPost } = this.props
     return (
@@ -107,13 +40,11 @@ class App extends Component {
             : (
               <Switch>
                 <Route exact path='/' component={Home} />
-                {/* TODO: if Category or ID doesn't exist -> 404 Page! */}
                 {
                   // check whether Category exists
                   isValidCategory &&
                     <Route exact path='/:category' component={Home} />
                 }
-                {/* <Route exact path='/:category' component={Home} /> */}
                 <Route path={`${prefixForPosts}:id/edit`} component={EditPost} />
                 <Route path={`/posts/add`} component={EditPost} />
                 {
@@ -137,15 +68,6 @@ function mapStateToProps({ categories, posts }, { location }) {
   const secondSlash = afterFirstSlash.indexOf('/')
   const paramCategory = afterFirstSlash.includes('/') ? afterFirstSlash.substring(0, secondSlash) : afterFirstSlash
   const paramId = afterFirstSlash.substring((secondSlash + 1))
-
-  // let isValidCategory = false
-  // categories.forEach(category => {
-  //   if(category.name === paramCategory){
-  //     isValidCategory = true
-  //   }
-  // })
-
-  console.log('Categories', categories.includes(paramCategory))
 
   return {
     isLoading: categories.length < 1 || posts.length < 1,
